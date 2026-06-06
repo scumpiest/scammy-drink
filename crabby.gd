2eae8010ef876f01ad7d3a40c98324ac9c72e768
scumpiest <saberfang961@gmail.com>
2026-06-07 00:00:59 +0200

    missing files

---
+++ crabby.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+signal request_completed
+signal request_failed
+
+@onready var bubble_background: PanelContainer = $BubbleBackground
+@onready var chat_bubble: Label = $BubbleBackground/ChatBubble
+
+var request: String = "cedevita"
+
+func _ready() -> void:
+	GameManager.crafting_complete.connect(_on_crafting_complete)
+
+func _on_crafting_complete(_recipe_key) -> void:
+	bubble_background.visible = true
+	if _recipe_key == request:
+		chat_bubble.text = "Thanks for the " + _recipe_key + "!"
+		await get_tree().create_timer(2.5).timeout
+		request_completed.emit()
+		bubble_background.visible = false
+		request = ""
+	else:
+		print("I DONT WANT THAT, GIVE ME " + request + "!!")
+		chat_bubble.text = "I DONT WANT THAT, GIVE ME " + request.to_upper() + "!!"
+		request_failed.emit()
