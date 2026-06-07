90d44fa400cecd26ce274b4fe3ce7bd5425518b9
scumpiest <saberfang961@gmail.com>
2026-06-07 18:24:48 +0200

    feat: initial fruit drag and drop implementation

---
+++ scenes/fruit_mix_2d.gd
@@ -0,0 +1,19 @@
+extends TextureButton
+
+@export var item_to_spawn: PackedScene
+@export var marker: Marker2D
+
+func _gui_input(event: InputEvent) -> void:
+	if not event is InputEventMouseButton:
+		return
+	if event.button_index != MOUSE_BUTTON_LEFT or not event.pressed:
+		return
+	if item_to_spawn == null:
+		print("Don't forget to assign an item scene in the Inspector!")
+		return
+
+	var new_item = item_to_spawn.instantiate()
+	new_item.global_position = get_global_mouse_position()
+	get_tree().current_scene.add_child(new_item)
+	new_item.drag_and_drop.begin_drag()
+	get_viewport().set_input_as_handled()
---
+++ scenes/ingredients/base_fruit.gd
@@ -0,0 +1,41 @@
+extends RigidBody2D
+
+@export var data: ItemData : set = _set_data
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var drag_and_drop: DragAndDrop = $DragAndDrop
+@onready var velocity_based_rotation: VelocityBasedRotation = $VelocityBasedRotation
+
+func _ready():
+	velocity_based_rotation.enabled = false
+	drag_and_drop.drag_started.connect(_on_drag_started)
+	drag_and_drop.dropped.connect(_on_dropped)
+	drag_and_drop.drag_canceled.connect(_on_drag_canceled)
+	if data:
+		_set_data(data)
+
+func _set_data(value: ItemData):
+	data = value
+
+	if not is_node_ready():
+		return
+
+	name = data.name
+	sprite.texture = data.sprite
+
+	# we need this for water modulation
+	# color = data.color
+
+func _on_drag_started() -> void:
+	velocity_based_rotation.enabled = true
+
+func _on_dropped(_starting_position: Vector2) -> void:
+	velocity_based_rotation.enabled = false
+
+func _on_drag_canceled(starting_position: Vector2) -> void:
+	velocity_based_rotation.enabled = false
+	_tween_to(starting_position)
+
+func _tween_to(target_position: Vector2) -> void:
+	var tween := create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	tween.tween_property(self, "global_position", target_position, 0.3)
