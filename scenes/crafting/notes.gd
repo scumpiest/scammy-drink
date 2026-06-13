b66253cbd725548c344998ba63441eb9ef6773f9
scumpiest <saberfang961@gmail.com>
2026-06-13 17:45:36 +0200

    feat: randomize ingredients on notes

--- scenes/crafting/notes.gd
+++ scenes/crafting/notes.gd
@@ -5,26 +5,47 @@ extends PanelContainer
 @onready var ingredient2: MarginContainer = $MarginContainer/VBoxContainer/VBoxContainer/Ingredient2
 @onready var ingredient3: MarginContainer = $MarginContainer/VBoxContainer/VBoxContainer/Ingredient3
 
+var random_chance: float = 0.0
+
 
 func update_display() -> void:
 	var order_data: Dictionary = get_order_data()
+
+	random_chance = order_data["random_chance"]
+
 	drink_name.update_from_order(order_data["recipe_key"])
-	ingredient1.update_from_order(order_data["ingredient1"])
-	ingredient2.update_from_order(order_data["ingredient2"])
-	ingredient3.update_from_order(order_data["ingredient3"])
+
+	update_ingredients(order_data["ingredients"])
 
 
 func get_order_data() -> Dictionary:
 	var recipe_key: String = GameManager.get_order()
 	var recipe_data: Dictionary = CraftingRecipe.crafting_dict[recipe_key]
 	var ingredients: Dictionary = recipe_data["ingredients"]
+	var ingredients_array: Array = ingredients.keys()
 
 	return {
 		"recipe_key": recipe_key,
-		"ingredient1": ingredients.keys()[0],
-		"ingredient2": ingredients.keys()[1],
-		"ingredient3": ingredients.keys()[2],
+		"ingredients": ingredients_array,
+		"random_chance": recipe_data["random_chance"],
 	}
+
+
+func update_ingredients(real_ingredients: Array) -> void:
+	var inventory_keys: Array = GameManager.inventory.keys()
+
+	var ingredient_lines: Array[MarginContainer] = [ingredient1, ingredient2, ingredient3]
+
+	for i in ingredient_lines.size():
+		var ingredient: String = ""
+		var random_ingredient: String = ""
+		if randf() < 0.5:
+			random_ingredient = inventory_keys[randi_range(0, inventory_keys.size() - 1)]
+			if not real_ingredients.has(random_ingredient):
+				ingredient = random_ingredient
+		else:
+			ingredient = real_ingredients[i]
+		ingredient_lines[i].update_from_order(ingredient)
 
 
 func try_scratch_at_position(global_pos: Vector2, replacement: String) -> bool:
--- scripts/crafting_recipe.gd
+++ scripts/crafting_recipe.gd
@@ -4,52 +4,62 @@ var crafting_dict: Dictionary = {}
 
 var cedevita_recipe: Dictionary = {
 	"ingredients": {"soda": 1, "orange": 1, "ice": 1},
-	"product": {"cedevita": 1}
+	"product": {"cedevita": 1},
+	"random_chance": 0.5
 }
 
 var pina_colada_recipe: Dictionary = {
 	"ingredients": {"coconut_cream": 1, "pineapple": 1, "ice": 1},
-	"product": {"pina_colada": 1}
+	"product": {"pina_colada": 1},
+	"random_chance": 0.5
 }
 
 var rebujito_recipe: Dictionary = {
 	"ingredients": {"soda": 1, "white_wine": 1, "mint": 1},
-	"product": {"rebujito": 1}
+	"product": {"rebujito": 1},
+	"random_chance": 0.5
 }
 
 var apfelschorle_recipe: Dictionary = {
 	"ingredients": {"apple": 1, "soda": 1, "ice": 1},
-	"product": {"apfelschorle": 1}
+	"product": {"apfelschorle": 1},
+	"random_chance": 0.5
 }
 
 var lemonade_recipe: Dictionary = {
 	"ingredients": {"water": 1, "lemon": 1, "mint": 1},
-	"product": {"lemonade": 1}
+	"product": {"lemonade": 1},
+	"random_chance": 0.5
 }
 
 var mojito_recipe: Dictionary = {
 	"ingredients": {"white_wine": 1, "lime_juice": 1, "soda": 1},
-	"product": {"mojito": 1}
+	"product": {"mojito": 1},
+	"random_chance": 0.5
 }
 
 var strawberry_milkshake_recipe: Dictionary = {
 	"ingredients": {"milk": 1, "strawberry": 1, "ice": 1},
-	"product": {"strawberry_milkshake": 1}
+	"product": {"strawberry_milkshake": 1},
+	"random_chance": 0.5
 }
 
 var coconut_mocktail_recipe: Dictionary = {
 	"ingredients": {"coconut_cream": 1, "lime_juice": 1, "ice": 1},
-	"product": {"coconut_mocktail": 1}
+	"product": {"coconut_mocktail": 1},
+	"random_chance": 0.5
 }
 
 var party_mix_recipe: Dictionary = {
 	"ingredients": {"white_wine": 1, "lime_juice": 1, "mixed_fruits": 1},
-	"product": {"party_mix": 1}
+	"product": {"party_mix": 1},
+	"random_chance": 0.5
 }
 
 var es_teler_recipe: Dictionary = {
 	"ingredients": {"milk": 1, "mixed_fruits": 1, "ice": 1},
-	"product": {"es_teler": 1}
+	"product": {"es_teler": 1},
+	"random_chance": 0.5
 }
 
 func _ready():
