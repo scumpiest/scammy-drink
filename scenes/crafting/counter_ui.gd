4f7626bcca005fb4c19beb49369e1a76a61a3249
scumpiest <saberfang961@gmail.com>
2026-06-16 17:37:00 +0200

    feat: dialogue system

--- scenes/crafting/counter_ui.gd
+++ scenes/crafting/counter_ui.gd
@@ -13,6 +13,7 @@ var current_target: Marker2D = null
 var _awaiting_notes_save: bool = false
 var _notes_saved_for_order: bool = false
 var customer: Customer
+var _dialogue_debug_enabled: bool = false
 
 var main_menu_scene: String = "uid://ctrh7huvrvaws"
 
@@ -24,6 +25,7 @@ func _ready() -> void:
 	SignalBus.notes_saved.connect(_on_notes_saved)
 	GameManager.all_recipes_three_star.connect(_on_all_recipes_three_star)
 	crafting_counter.drink_ingredient_added.connect(capy_notes.add_ingredient)
+	crafting_counter.drink_ingredient_added.connect(_on_drink_ingredient_added)
 	notes.drink_cleared.connect(capy_notes.clear_ingredients)
 	customer = spawn_customer()
 	setup_customer(customer)
@@ -38,6 +40,8 @@ func _input(event: InputEvent) -> void:
 		recipe_book.visible = false
 	if event.is_action_pressed("open_recipe"):
 		recipe_book.visible = true
+	if event.is_action_pressed("toggle_dialogue_debug"):
+		_toggle_dialogue_debug_mode()
 
 func spawn_customer() -> Customer:
 	var new_customer: Customer = customer_scene.instantiate() as Customer
@@ -50,6 +54,7 @@ func spawn_customer() -> Customer:
 func setup_customer(new_customer: Customer) -> void:
 	new_customer.request_completed.connect(_on_request_completed)
 	new_customer.request_failed.connect(_on_request_failed)
+	new_customer.set_dialogue_debug_enabled(_dialogue_debug_enabled)
 	_notes_saved_for_order = not GameManager.get_session_notes(GameManager.get_order()).is_empty()
 	notes.update_display()
 	slide_to_marker(enter_marker)
@@ -101,3 +106,35 @@ func _on_recipe_button_pressed() -> void:
 # TODO: Add a victory screen
 func _on_all_recipes_three_star() -> void:
 	recipe_book.visible = true
+
+
+func _on_drink_ingredient_added(ingredient: String) -> void:
+	if not customer or not is_instance_valid(customer):
+		return
+	if customer.request.is_empty():
+		return
+
+	var recipe_data: Dictionary = CraftingRecipe.crafting_dict.get(customer.request, {})
+	if recipe_data.is_empty():
+		return
+
+	var recipe_ingredients: Dictionary = recipe_data.get("ingredients", {})
+	var is_wrong: bool = not recipe_ingredients.has(ingredient)
+	var total_added: int = crafting_counter.crafting_ingredients.size()
+	var missing_hint: String = _get_missing_ingredient_hint(recipe_ingredients)
+
+	customer.on_ingredient_feedback(ingredient, is_wrong, total_added, missing_hint)
+
+
+func _get_missing_ingredient_hint(recipe_ingredients: Dictionary) -> String:
+	for recipe_ingredient in recipe_ingredients.keys():
+		if not crafting_counter.crafting_ingredients.has(recipe_ingredient):
+			return str(recipe_ingredient)
+	return ""
+
+
+func _toggle_dialogue_debug_mode() -> void:
+	_dialogue_debug_enabled = not _dialogue_debug_enabled
+	print("[DialogueDebug] enabled=%s" % str(_dialogue_debug_enabled))
+	if customer and is_instance_valid(customer):
+		customer.set_dialogue_debug_enabled(_dialogue_debug_enabled)
