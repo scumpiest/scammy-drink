ab08866741714f1686dc7e193515c439c1a53b78
starry-nites <jacjack09@outlook.com>
2026-06-16 15:09:13 -0500

    image in right place. Requires testing

--- scenes/crafting/crafting_complete.gd
+++ scenes/crafting/crafting_complete.gd
@@ -2,8 +2,9 @@ class_name CraftingComplete
 
 extends Sprite2D
 
-var base_path: String = "res://assets/Drinks/"
-@onready var drink_marker: Marker2D = $DrinkMarker
+var base_path: String = "res://assets/drinks/"
+#
+@onready var drink_marker: Marker2D = get_parent().get_parent().find_child("DrinkMarker", true, false)
 static var png_path: String
 
 var img = Image.new()
@@ -11,12 +12,13 @@ var img = Image.new()
 # Called when the node enters the scene tree for the first time.
 # res://assets/drinks/Apfelschorle.png
 func _ready() -> void:
+	print(get_parent())
 	var full_path: String = base_path + png_path
 	img.load_from_file(full_path)
 	
 	var img_texture: CompressedTexture2D = load(full_path)
 	texture = img_texture
-	
+	print(self, drink_marker)
 	position.x = drink_marker.position.x
 	position.y = drink_marker.position.y
 
