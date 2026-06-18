a6fb383233a4f91f795f9f7c086cfdec7bcead76
Juca-5 <Joline.cezanne@gmx.de>
2026-06-19 01:09:00 +0200

    settings UI

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var settings_panel = $MarginContainer/SettingsMenu
 var play_scene: String = "uid://omqlt0501aof"
 var settings_scene: String = "uid://c86lgs72w0q84"
 # Called when the node enters the scene tree for the first time.
@@ -17,7 +18,9 @@ func _on_play_pressed() -> void:
 	SceneManager.switch_scene(play_scene)
 
 func _on_settings_pressed() -> void:
-	SceneManager.switch_scene(settings_scene)
+	settings_panel.visible = true 
+	settings_panel.z_index = 10
+	mouse_filter = Control.MOUSE_FILTER_IGNORE
 
 
 func _on_quit_pressed() -> void:
